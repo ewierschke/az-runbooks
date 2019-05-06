017dba0e367d1edc3e3d3ed9811b9b35c8054098
Erik Wierschke <erik.wierschke@plus3it.com>
2019-05-06 08:42:03 -0400

    add check for preexisting dest files

--- copybetweenstgaccts.ps1
+++ copybetweenstgaccts.ps1
@@ -3,6 +3,7 @@
    Copy blobs in a container from the one storage account to a different storage account.  Updates blob if source has newer version. 
 .DESCRIPTION 
    Start's an asynchronous copy of blob to a different storage account. 
+   Appears to create a 7 day read SAS for the copy operation or get status operation
 .EXAMPLE 
    .\copybetweenstgaccts.ps1  
          -azSubscription "Azure Subscription"  
@@ -133,7 +134,7 @@ foreach($blob in $blobsArray) {
         ### Check the current status of the copy operation ###
         $status = Get-AzStorageBlobCopyState -Blob $blob.Name -Container $containerName -Context $destContext
         ### Print out status ### 
-        Write-Output $status 
+        #Write-Output $status 
         ### Loop until complete ###                                    
         While($status.Status -eq "Pending"){
             $status = Get-AzStorageBlobCopyState -Blob $blob.Name -Container $containerName -Context $destContext
@@ -141,9 +142,10 @@ foreach($blob in $blobsArray) {
             ### display the formatted status information 
             (Get-Date).ToString() + ":" + ( "{0: P0}"  -f ( $status.BytesCopied / $status.TotalBytes))
             ### Print out status ###
-            Write-Output $status
+            #Write-Output $status
+            Write-Output "Waiting for long running copy to complete..." 
         }
-        Write-Output "Long Running Azure Copy Successfully Complete!"
+        Write-Output "Long Running Blob Copy Successfully Complete!"
     } catch {
         # Report any other error
         Write-Error $Error[0].Exception;
@@ -168,7 +170,7 @@ foreach($blob in $blobsArray) {
         ### Check the current status of the copy operation ###
         $status = Get-AzStorageBlobCopyState -Blob $blob.Name -Container $containerName -Context $destContext
         ### Print out status ### 
-        Write-Output $status 
+        #Write-Output $status 
         ### Loop until complete ###                                    
         While($status.Status -eq "Pending"){
             $status = Get-AzStorageBlobCopyState -Blob $blob.Name -Container $containerName -Context $destContext
@@ -176,9 +178,25 @@ foreach($blob in $blobsArray) {
             ### display the formatted status information 
             (Get-Date).ToString() + ":" + ( "{0: P0}"  -f ( $status.BytesCopied / $status.TotalBytes))
             ### Print out status ###
-            Write-Output $status
+            #Write-Output $status
+            Write-Output "Waiting for long running copy to complete..." 
         }
-        Write-Output "Long Running Azure Copy Successfully Complete!"
+        Write-Output "Long Running Blob Copy Successfully Complete!"
     }
 }
+Write-Output "##########"
 Write-Output "Script Complete.  ${newfiles} New files copied to container, ${updatedfiles} Files updated to container"
+
+#display a list of files that exist in destination but not source
+$destblobsArray = Get-AzStorageBlob -Container $containerName -Context $destContext
+$onlyindest = Compare-Object -ReferenceObject $blobsArray.Name -DifferenceObject $destblobsArray.Name -Passthru
+if ($onlyindest) {
+    Write-Output "##########"
+    Write-Output "***The following files were pre-existing in the destination but were not in the source."
+    Write-Output "***Investigate whether the files are still needed." 
+    foreach($destblob in $onlyindest) {
+        $destblobname = $destblob
+        Write-Output "- $destblobname"
+    }
+    Write-Output "##########"
+}
